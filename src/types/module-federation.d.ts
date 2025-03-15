322f3af278a9a490573a1e7d5bfef81a52e4d16d
Enrique Trigoso <enrique.el.capo@gmail.com>
2025-03-15 18:35:16 -0500

    feat: configure module federation and exposing pie chart component

---
+++ src/types/module-federation.d.ts
@@ -0,0 +1,4 @@
+declare module "dashboard/Header";
+declare module "dashboard/Sidebar";
+declare module "dashboard/DashboardLayout";
+declare module "dashboard/DashboardPage";
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,31 @@
 import { fileURLToPath, URL } from "node:url";
-
+import federation from "@originjs/vite-plugin-federation";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    target: "esnext",
     commonjsOptions: {
       include: ["tailwind.config.js", "node_modules/**"],
     },
+    minify: false,
+    cssCodeSplit: false,
   },
   optimizeDeps: {
     include: ["tailwind-config"],
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./PieChart": "./src/components/PieChart/index.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
